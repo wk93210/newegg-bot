6fd8b790a799cbd13558046faa1d7c192803ab7b
Ataraxoa <ajdoug23@gmail.com>
2020-10-08 01:55:24 -0400

    Fixed for checkout when searching for multiple cards and one comes in stock

--- NeweggBot.js
+++ NeweggBot.js
@@ -5,6 +5,25 @@ async function report (log) {
 	currentTime = new Date();
 	console.log(currentTime.toString().split('G')[0] + ': ' + log)
 }
+async function check_cart (page) {
+	await page.waitForTimeout(500)
+	try {
+		//await page.waitForSelector('span.amount')
+		var element = await page.$('span.amount')
+		await report("Card in stock, attempting to purchase")
+		var text = await page.evaluate(element => element.textContent, element);
+		if (parseInt(text.split('$')[1]) > config.price_limit) {
+			await report("Price exceeds limit but bot is currently not working correctly with this functionality")
+			return false
+		}
+		return true
+	} catch (err) {
+		await report("Card not in stock")
+		await page.waitForTimeout(config.refresh_time * 1000)
+		return false
+	}
+}
+
 
 async function run () {
 	await report("Started")
@@ -12,7 +31,6 @@ async function run () {
         	headless: false,
         	defaultViewport: { width: 1366, height: 768 }
     	})
-	
     	const page = await browser.newPage()
 	
 	await page.goto('http://newegg.com', { waitUntil: 'networkidle2' })
@@ -53,28 +71,21 @@ async function run () {
 	{
 		await page.goto('https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=' + config.item_number, { waitUntil: 'load' })
 		if (page.url().includes("ShoppingCart")) {
-			await report("Card not in stock")
-			await page.waitForTimeout(config.refresh_time * 1000)
-		} else if (page.url().includes("ShoppingItem")) {
-			await report("Card in stock, attempting to purchase")
-			await page.goto('https://secure.newegg.com/Shopping/ShoppingCart.aspx', { waitUntil: 'networkidle2' })
-			await page.waitForSelector('span.amount')
-			const element = await page.$('span.amount')
-			const text = await page.evaluate(element => element.textContent, element);
-			if (parseInt(text.split('$')[1]) > config.price_limit) {
-				await report("Price exceeds limit")
-				try {
-					await page.goto("javascript:attachDelegateEvent((function(){Biz.GlobalShopping.ShoppingCart.shoppingCartOperation('14-126-352.1.0.0','REMOVECHKITEMS','');}))", {timeout: 500})
-				} catch (err) {
-				}
-				continue
+			var check = await check_cart(page)
+			if (check) {
+				break
 			}
-			break
+		} else if (page.url().includes("ShoppingItem")) {
+			await page.goto('https://secure.newegg.com/Shopping/ShoppingCart.aspx', { waitUntil: 'networkidle2' })
+			await page.waitForTimeout(500)
+			var check = await check_cart(page)
+			if (check){
+				break
+			}
 		} else if (page.url().includes("areyouahuman")) {
 			await page.waitForTimeout(1000)
 		}
 	}
-	
 	try {
 		await page.goto('javascript:attachDelegateEvent((function(){Biz.GlobalShopping.ShoppingCart.checkOut(\'True\')}))', {timeout: 500})
 	} catch (err) {
